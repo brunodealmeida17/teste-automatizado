fc9e5d5b035aa3cb96c7a3c61522e1425ae465ba
Bruno de Almeida <brunodealmeida17@yahoo.com>
2022-06-27 18:23:21 -0300

    projeto finalizado

--- automacao/teste.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("./automacao/links.json", encoding="utf-8") as arquivo:
-    dados = json.load(arquivo)
-    
-oi = dados["sensorWeb"]["xpath_como_funciona"]["xpath_midia"]
-
-casa = oi
-print(casa)
